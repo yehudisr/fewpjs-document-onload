8041e3da9a1ca99ad611327e903e1780f2a30df3
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-03-17 18:20:46 -0400

    commit tree b3d4793c5b264528a42a2a537e5ccc39c42551fb

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  text = document.querySelector('p#text')
+  text.textContent = "This is really cool!"
+}); 
+
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  ); 
